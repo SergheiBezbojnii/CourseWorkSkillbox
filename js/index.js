675647ad6a21d787ca931299b27e258dd830f80c
Serghei <sergheibezbojnii@gmail.com>
2022-03-03 19:02:47 +0200

    broadcasts

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// header-burger
+
 const burgerBtn = document.querySelector('.header-center__burger');
 const menuClose = document.querySelector('.header__close-menu');
 const menuBurger = document.querySelector('.header__nav');
@@ -9,3 +11,15 @@ burgerBtn.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
   menuBurger.classList.remove('burger-active');
 });
+
+// select
+
+(function ($) {
+  $(function () {
+
+    $('select').styler();
+
+  });
+})(jQuery);
+
+
