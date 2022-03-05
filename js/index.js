45341eb98cecf3ec8e5287312d2e1f37a48ab9cd
Serghei <sergheibezbojnii@gmail.com>
2022-03-05 10:32:29 +0200

    guests

--- js/index.js
+++ js/index.js
@@ -22,4 +22,81 @@ menuClose.addEventListener('click', () => {
   });
 })(jQuery);
 
+// catalog
+
+function openCatalogEl() {
+  document.querySelectorAll('.accordion__value-item').forEach(el => {
+    const link = el.querySelector('.accordion__link');
+
+    if (window.matchMedia("(max-width: 868px)").matches) {
+      link.href = '#autorSpace'
+    }
+
+    el.addEventListener('click', () => {
+      const valueEl = el.textContent;
+
+      const listAutor = document.querySelectorAll('.autor');
+
+      for (let i of listAutor) {
+        const autorName = i.querySelector('.autor__name').textContent;
+
+        document.querySelector('.autor_active').classList.remove('autor_active');
+
+        if (valueEl === autorName.trim()) {
+          i.classList.add('autor_active');
+          break
+        }
+
+        document.querySelector('.autor_none').classList.add('autor_active');
+      }
+    })
+  })
+}
+
+openCatalogEl()
+
+/*функционал аккордеона*/
+
+function accordion() {
+  let interval = null;
+  document.querySelectorAll('.accordion__item').forEach(el => {
+    el.querySelector('.accordion__row').addEventListener('click', () => {
+      if (!el.classList.contains('accordion_action') && document.querySelector('.accordion_action')) {
+        document.querySelectorAll('.accordion_action').forEach(el => {
+          el.classList.remove('accordion_action');
+          const value = el.querySelector('.accordion__row-value');
+          const valueList = el.querySelector('.accordion__value');
+
+          value.style.maxHeight = null;
+
+          if (valueList.style.visibility === 'visible') {
+            interval = setTimeout(() => {
+              valueList.style.visibility = 'hidden';
+            }, 100);
+          }
+        })
+      }
+
+      el.classList.toggle('accordion_action');
+
+      const valueList = el.querySelector('.accordion__value');
+      const value = el.querySelector('.accordion__row-value');
+
+      if (valueList.style.visibility === 'visible') {
+        interval = setTimeout(() => {
+          valueList.style.visibility = 'hidden';
+        }, 100);
+      } else {
+        valueList.style.visibility = 'visible';
+      }
+
+      if (value.style.maxHeight){
+        value.style.maxHeight = null;
+      } else {
+        value.style.maxHeight = value.scrollHeight + "px";
+      }
+    })
+  })
+}
+
 
